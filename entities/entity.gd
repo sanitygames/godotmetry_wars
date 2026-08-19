9a20bedf07f3fe6038bc57edde2da4063893c76a
sanitygames0436 <sanitygames04@gmail.com>
2026-08-19 09:33:32 +0900

    refactor: remove godotplayer_score addon

--- entities/entity.gd
+++ entities/entity.gd
@@ -9,25 +9,27 @@ signal shoot(id: int)
 const DEF_VEC2 := Vector2(9999, 9999)
 
 @export var max_life := 1
-
 var id := 0
 var life := 1
 
 @abstract func move(delta: float, _d: Dictionary = {}) -> Vector2
 
 
+## 生成時
 func initialize(_id: int) -> void:
 	position = DEF_VEC2
 	id = _id
 	set_deferred("monitoring", false)
 
 
+## アクティブ開始時
 func spawn(pos: Vector2, _rot: float = 0.0) -> void:
 	life = max_life
 	position = pos
 	set_deferred("monitoring", true)
 	
 
+## アクティブ終了時
 func death() -> void:
 	set_deferred("monitoring", false)
 	position = DEF_VEC2
